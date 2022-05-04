9fa081fb91bad8d7bd24131f10a76ddf90f0390c
Vity <vity.1215@gmail.com>
2022-05-04 21:48:11 +0800

    http post json with header
    Signed-off-by: Vity <vity.1215@gmail.com>

--- kit/http.go
+++ kit/http.go
@@ -76,6 +76,31 @@ func HttpPostJson(requestUrl string, json string) (bool, string, int) {
 	return true, string(body), resp.StatusCode
 }
 
+// HttpPostJsonWithHeader 执行Post JSON请求
+func HttpPostJsonWithHeader(requestUrl string, json string, headers map[string]string) (bool, string, int) {
+	req, err := http.NewRequest("POST", requestUrl, bytes.NewBuffer([]byte(json)))
+	if err != nil {
+		return false, fmt.Sprintf("[%s] %s", "k.HttpPostJson", "Network Create Error："+err.Error()), 0
+	}
+	client := http.Client{}
+	req.Header.Set("Content-Type", "application/json")
+	for key, val := range headers {
+		req.Header.Set(key, val)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, fmt.Sprintf("[%s] %s", "k.HttpPostJson", "Network Request Error："+err.Error()), 0
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, fmt.Sprintf("[%s] %s", "k.HttpPostJson", "Network Response Error："+err.Error()), 0
+	}
+	return true, string(body), resp.StatusCode
+}
+
 // HttpPostXmlSecure 执行Post XML 证书请求
 func HttpPostXmlSecure(requestUrl string, xml string, certFile string, keyFile string, rootCaFile string) (bool, string, int) {
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)

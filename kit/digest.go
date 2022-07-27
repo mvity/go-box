48ecbf9d8cbe386f923cc061c8fd904471a6202f
Vity <vity.1215@gmail.com>
2022-07-27 23:04:23 +0800

    digest
    Signed-off-by: Vity <vity.1215@gmail.com>

--- kit/digest.go
+++ kit/digest.go
@@ -7,6 +7,7 @@ package k
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
 )
@@ -16,6 +17,13 @@ func MD5String(plain string) string {
 	hMD5 := md5.New()
 	hMD5.Write([]byte(plain))
 	return hex.EncodeToString(hMD5.Sum(nil))
+}
+
+// SHA1String  对字符串SHA1处理
+func SHA1String(plain string) string {
+	hSha1 := sha1.New()
+	hSha1.Write([]byte(plain))
+	return hex.EncodeToString(hSha1.Sum(nil))
 }
 
 // SHA256String  对字符串SHA256处理

e9e0e94b423905b7e86aa5e2828d7d65512ec92c
Vity <vity.1215@gmail.com>
2022-12-08 21:27:44 +0800

    1.0.1
    Signed-off-by: Vity <vity.1215@gmail.com>

--- x/string.go
+++ x/string.go
@@ -35,3 +35,71 @@ func StringIsAllBlank(value ...string) bool {
 	}
 	return true
 }
+
+// StringIsNotBlank 判断给定的字符串是否为空字符串
+func StringIsNotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+// StringIsBlank 判断给定的字符串是否为空字符串
+func StringIsBlank(value string) bool {
+	return strings.TrimSpace(value) == ""
+}
+
+// StringDefaultIfBlank 返回字符串，若给定的字符串为空，则返回默认字符串
+func StringDefaultIfBlank(value, defaultString string) string {
+	if StringIsBlank(value) {
+		return defaultString
+	}
+	return value
+}
+
+// StringLeftPad 左补齐给定字符串
+func StringLeftPad(value string, size int, padString string) string {
+	padLen := len(padString)
+	strLen := len(value)
+	pads := size - strLen
+	if pads <= 0 {
+		return value
+	}
+	if padLen == 1 {
+		return strings.Repeat(padString, pads) + value
+	}
+	if pads == padLen {
+		return padString + value
+	}
+	if pads < padLen {
+		return padString[:(pads)] + value
+	}
+	padding := make([]rune, pads)
+	padRunes := []rune(padString)
+	for i := 0; i < pads; i++ {
+		padding[i] = padRunes[i%padLen]
+	}
+	return string(padding) + value
+}
+
+// StringRightPad 右补齐给定字符串
+func StringRightPad(value string, size int, padString string) string {
+	padLen := len(padString)
+	strLen := len(value)
+	pads := size - strLen
+	if pads <= 0 {
+		return value
+	}
+	if padLen == 1 {
+		return value + strings.Repeat(padString, pads)
+	}
+	if pads == padLen {
+		return value + padString
+	}
+	if pads < padLen {
+		return value + padString[:(pads)]
+	}
+	padding := make([]rune, pads)
+	padRunes := []rune(padString)
+	for i := 0; i < pads; i++ {
+		padding[i] = padRunes[i%padLen]
+	}
+	return value + string(padding)
+}

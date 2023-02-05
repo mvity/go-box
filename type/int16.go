a807692a60c3b0cb794629c7bd7874c98323647e
Vity <vity.1215@gmail.com>
2023-02-05 22:39:02 +0800

    1.0.3
    Signed-off-by: Vity <vity.1215@gmail.com>

--- type/int16.go
+++ type/int16.go
@@ -1,4 +1,4 @@
-// Copyright © 2021 - 2022 vity <vityme@icloud.com>.
+// Copyright © 2021 - 2023 vity <vityme@icloud.com>.
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.

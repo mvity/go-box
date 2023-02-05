f9354442b65f275166e4e54da0505297a8a0868d
vity <vity.1215@gmail.com>
2023-02-05 22:44:48 +0800

    1.0.3

--- type/string.go
+++ type/string.go
@@ -1,7 +1,9 @@
-// Copyright © 2021 - 2023 vity <vityme@icloud.com>.
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
+/*
+ * Copyright © 2021 - 2023 vity <vityme@icloud.com>.
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file.
+ */
 
 package t
 

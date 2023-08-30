5762290953bb97a3a2281205b95037798ecdc07e
vity <vity.1215@gmail.com>
2023-08-30 15:52:08 +0800

    变更

--- examples/test/test.go
+++ examples/test/test.go
@@ -10,117 +10,19 @@ package main
 import (
 	"fmt"
 	"github.com/mvity/go-box/x"
+	"time"
 )
 
 func main() {
 
-	p := x.NewGeoPoint("113.685214,34.789401")
-	ps := x.NewGeoPoints("113.736638,34.842372:113.615533,34.843031:113.501244,34.810442:113.503647,34.737158:113.705633,34.611918:113.849389,34.772623")
-	fmt.Println(x.GeoCheckInAreas(p, ps))
-	//
-	//runReqs()
-}
+	now := time.Date(2023, 8, 31, 0, 0, 0, 0, time.Now().Location())
 
-func runReqs() {
-	//var res *http.Response
-	//var body []byte
-	//var err error
-	//res = doApi1()
-	//defer func(Body io.ReadCloser) {
-	//	_ = Body.Close()
-	//}(res.Body)
-	//body, err = io.ReadAll(res.Body)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(string(body))
-	//
-	//res = doApi2()
-	//defer func(Body io.ReadCloser) {
-	//	_ = Body.Close()
-	//}(res.Body)
-	//body, err = io.ReadAll(res.Body)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(string(body))
-	//
-	//res = doApi3()
-	//defer func(Body io.ReadCloser) {
-	//	_ = Body.Close()
-	//}(res.Body)
-	//body, err = io.ReadAll(res.Body)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println(string(body))
+	fmt.Println(x.GetWeekdayOfDay(now, time.Monday))
+	fmt.Println(x.GetWeekdayOfDay(now, time.Tuesday))
+	fmt.Println(x.GetWeekdayOfDay(now, time.Wednesday))
+	fmt.Println(x.GetWeekdayOfDay(now, time.Thursday))
+	fmt.Println(x.GetWeekdayOfDay(now, time.Friday))
+	fmt.Println(x.GetWeekdayOfDay(now, time.Saturday))
+	fmt.Println(x.GetWeekdayOfDay(now, time.Sunday))
 
 }
-
-//func doPost(uri string, headers map[string]string, body string) *http.Response {
-//	proxy, _ := url.Parse("http://127.0.0.1:8888")
-//	client := http.Client{
-//		Transport: &http.Transport{
-//			Proxy:       http.ProxyURL(proxy),
-//			DialContext: (&net.Dialer{KeepAlive: 30 * time.Second}).DialContext,
-//		},
-//	}
-//	t1 := time.Now()
-//	req, err := http.NewRequest("POST", uri, bytes.NewBuffer([]byte(body)))
-//	if err != nil {
-//		println(err)
-//	}
-//	for key, val := range headers {
-//		req.Header.Set(key, val)
-//	}
-//
-//	res, err := client.Do(req)
-//	t2 := time.Now().Sub(t1)
-//	if err != nil {
-//		println(err)
-//	}
-//	fmt.Println("Req ", uri, "time : ", t2)
-//	return res
-//}
-//
-//func doApi1() *http.Response {
-//	uri := ""
-//	headers := map[string]string{
-//		"User-Agent":      "UnityPlayer/2020.3.34f1 (UnityWebRequest/1.0, libcurl/7.80.0-DEV)",
-//		"Accept":          "*/*",
-//		"Accept-Encoding": "deflate, gzip",
-//		"Context-Type":    "application/octet-stream",
-//		"x_acts":          "",
-//		"X-Unity-Version": "2020.3.34f1",
-//	}
-//	body := ""
-//	return doPost(uri, headers, body)
-//}
-//
-//func doApi2() *http.Response {
-//	uri := ""
-//	headers := map[string]string{
-//		"User-Agent":      "UnityPlayer/2020.3.34f1 (UnityWebRequest/1.0, libcurl/7.80.0-DEV)",
-//		"Accept":          "*/*",
-//		"Accept-Encoding": "deflate, gzip",
-//		"Context-Type":    "application/octet-stream",
-//		"x_acts":          "",
-//		"X-Unity-Version": "2020.3.34f1",
-//	}
-//	body := ""
-//	return doPost(uri, headers, body)
-//}
-//
-//func doApi3() *http.Response {
-//	uri := ""
-//	headers := map[string]string{
-//		"User-Agent":      "UnityPlayer/2020.3.34f1 (UnityWebRequest/1.0, libcurl/7.80.0-DEV)",
-//		"Accept":          "*/*",
-//		"Accept-Encoding": "deflate, gzip",
-//		"Context-Type":    "application/octet-stream",
-//		"x_acts":          "",
-//		"X-Unity-Version": "2020.3.34f1",
-//	}
-//	body := ""
-//	return doPost(uri, headers, body)
-//}
--- x/time.go
+++ x/time.go
@@ -7,14 +7,32 @@
 
 package x
 
-import "time"
+import (
+	"time"
+)
 
 // GetZeroTime  获取指定时间零点时刻
 func GetZeroTime(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
-// GetLastTime  获取指定时间最后时刻
+// GetLastTime  获取指定时间最后时刻 23:59:59
 func GetLastTime(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 0, t.Location())
+}
+
+// GetWeekdayOfDay 获取指定日期本周周几的日期
+func GetWeekdayOfDay(t time.Time, weekday time.Weekday) time.Time {
+	if t.Weekday() == weekday {
+		return GetZeroTime(t)
+	}
+	offset := int(weekday - t.Weekday())
+	if t.Weekday() == time.Sunday {
+		offset = 0 - (7 - offset)
+	}
+	day := GetZeroTime(t.AddDate(0, 0, offset))
+	if weekday == time.Sunday {
+		day = day.AddDate(0, 0, 7)
+	}
+	return day
 }
